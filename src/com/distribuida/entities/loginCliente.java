551a660390e6721a2fe814597ea9cf97bc2cd1fd
MATUTINO <MATUTINO@DESKTOP-FFBVPJ2>
2024-01-31 12:20:07 -0500

    000

--- src/com/distribuida/entities/loginCliente.java
+++ src/com/distribuida/entities/loginCliente.java
@@ -17,88 +17,133 @@ import org.springframework.stereotype.Component;
 @Table(name = "loginCliente")
 
 public class loginCliente {
+	
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+	
+  @Column(name = "id")
+     private int id;
+   @Column(name = "user")
+	private String user;
+   @Column(name = "password")
+	private String password;
+    @Column(name = "newpassword")
+	private String newpassword;
+    @Column(name = "confirmpassword")
+@Column(name = "confirmpassword")
+	private String confirmpassword;
+	@Column(name = "logincorreo")
+	private String loginCorreo;
+	@Column(name = "logintoken")
+    private String loginToken;
+	@Column(name = "SessiontimeIn")
+    private String SessiontimeIn;
+	@Column(name = "SessiontimeOUT")
+    private String SessiontimeOUT;
+	@Column(name = "Sessiontiempo")
+    private String  Sessiontiempo;
+    
+	
+	
+	public loginCliente () {
+		
+		
+	}
+	
 
 	
-	private int idCliente;
-	private String cedula;
-	private String nombre;
-    private String apellido;
-    private String correo;
-	private String user;
-	private String password;
-	private int edad;
-	private String direccion;
-	private String telefono;
-	 
-	public loginCliente () {}
+public loginCliente (int id,String user,String password,String newpassword,String confirmpassword,
+		String loginCorreo,String loginToken,String SessiontimeIn,String SessiontimeOUT,String  Sessiontiempo) {
 	
 	
-	public int getIdCliente() {
-		return idCliente;
+}
+
+	
+	
+	public int getId() {
+		return id;
 	}
-	public void setIdCliente(int idCliente) {
-		this.idCliente = idCliente;
+
+	public void setId(int id) {
+		this.id = id;
 	}
-	public String getCedula() {
-		return cedula;
-	}
-	public void setCedula(String cedula) {
-		this.cedula = cedula;
-	}
-	public String getNombre() {
-		return nombre;
-	}
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-	public String getApellido() {
-		return apellido;
-	}
-	public void setApellido(String apellido) {
-		this.apellido = apellido;
-	}
-	public String getCorreo() {
-		return correo;
-	}
-	public void setCorreo(String correo) {
-		this.correo = correo;
-	}
+
 	public String getUser() {
 		return user;
 	}
+
 	public void setUser(String user) {
 		this.user = user;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	public int getEdad() {
-		return edad;
+
+	public String getNewpassword() {
+		return newpassword;
 	}
-	public void setEdad(int edad) {
-		this.edad = edad;
+
+	public void setNewpassword(String newpassword) {
+		this.newpassword = newpassword;
 	}
-	public String getDireccion() {
-		return direccion;
+
+	public String getConfirmpassword() {
+		return confirmpassword;
 	}
-	public void setDireccion(String direccion) {
-		this.direccion = direccion;
+
+	public void setConfirmpassword(String confirmpassword) {
+		this.confirmpassword = confirmpassword;
 	}
-	public String getTelefono() {
-		return telefono;
+
+	public String getLoginCorreo() {
+		return loginCorreo;
 	}
-	public void setTelefono(String telefono) {
-		this.telefono = telefono;
+
+	public void setLoginCorreo(String loginCorreo) {
+		this.loginCorreo = loginCorreo;
+	}
+
+	public String getLoginToken() {
+		return loginToken;
+	}
+
+	public void setLoginToken(String loginToken) {
+		this.loginToken = loginToken;
+	}
+
+	public String getSessiontimeIn() {
+		return SessiontimeIn;
+	}
+
+	public void setSessiontimeIn(String sessiontimeIn) {
+		SessiontimeIn = sessiontimeIn;
+	}
+
+	public String getSessiontimeOUT() {
+		return SessiontimeOUT;
+	}
+
+	public void setSessiontimeOUT(String sessiontimeOUT) {
+		SessiontimeOUT = sessiontimeOUT;
+	}
+
+	public String getSessiontiempo() {
+		return Sessiontiempo;
+	}
+
+	public void setSessiontiempo(String sessiontiempo) {
+		Sessiontiempo = sessiontiempo;
 	}
 	@Override
 	public String toString() {
-		return "loginCliente [idCliente=" + idCliente + ", cedula=" + cedula + ", nombre=" + nombre + ", apellido="
-				+ apellido + ", correo=" + correo + ", user=" + user + ", password=" + password + ", edad=" + edad
-				+ ", direccion=" + direccion + ", telefono=" + telefono + "]";
+		return "loginCliente [id=" + id + ", user=" + user + ", password=" + password + ", newpassword=" + newpassword
+				+ ", confirmpassword=" + confirmpassword + ", loginCorreo=" + loginCorreo + ", loginToken=" + loginToken
+				+ ", SessiontimeIn=" + SessiontimeIn + ", SessiontimeOUT=" + SessiontimeOUT + ", Sessiontiempo="
+				+ Sessiontiempo + "]";
 	}
 }
-
-
